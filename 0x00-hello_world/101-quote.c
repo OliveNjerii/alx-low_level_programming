c3e85f2ad96123fcdec2bcc0099a2c5c43a1d295
OliveNjerii <njeriolive35@gmail.com>
2023-06-15 11:38:24 -0700

    modified file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+int main() {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, strlen(message));
+    return 1;
 }
+

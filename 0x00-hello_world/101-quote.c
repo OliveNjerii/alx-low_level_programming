8a774af5adf03fd9b92f4efef34884997c271fad
OliveNjerii <njeriolive35@gmail.com>
2023-06-15 11:40:25 -0700

    modified file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
 #include <string.h>
 
-int main() {
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void) {
     const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, strlen(message));
-    return 1;
+    return (1);
 }
 

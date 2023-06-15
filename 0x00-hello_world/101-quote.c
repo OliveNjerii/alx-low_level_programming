e6bddb29fd374b8f3282df51c5d4847e5a107f68
OliveNjerii <njeriolive35@gmail.com>
2023-06-15 11:46:48 -0700

    modified file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,12 @@
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = sizeof(message) - 1;
 
-	write(2, message, strlen(message));
+	write(2, message, len);
 
 	return (1);
 }
+
 
 

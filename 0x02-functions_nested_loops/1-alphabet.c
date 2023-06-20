277fd45746a39e4e6a0fb411d9b76502f197c9ea
OliveNjerii <njeriolive35@gmail.com>
2023-06-20 12:55:55 -0700

    prints lowercase letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * print_alphabet - prints lowercase letters
+ *
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{	print_alphabet();
+
+	putchar ('\n');
+
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar (letter);
+		letter++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,6 @@
 
 ## Script 0
 - prints _putchar, followed by a new line
+
+## Script 1
+- function that prints the alphabet, in lowercase, followed by a new line.

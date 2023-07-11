b77db46465447e463210fed1b53b7fe2f6591d70
OliveNjerii <njeriolive35@gmail.com>
2023-07-10 23:39:52 -0700

    copies a string from one mem location to another

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - Creates a duplicate of a string.
+ * @str: The string to be duplicated.
+ *
+ * Return: A pointer to the duplicated string, or NULL if it fails.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i, length;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	length = strlen(str);
+	s = malloc(sizeof(char) * length);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < length; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,6 @@
 
 ## createarray.c
 - creates an array of chars, and initializes it with a specific char.
+
+## copyarray.c
+- copies a string from one mem location to another.
